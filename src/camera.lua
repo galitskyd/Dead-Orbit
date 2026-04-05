5c06e0c141f4efd3f2c5c8f4ebf39dbddf2c2b08
David Galitsky <galitskyd@gmail.com>
2026-04-05 14:36:04 -0400

    better camera

--- src/camera.lua
+++ src/camera.lua
@@ -7,8 +7,16 @@ end
 
 function update_camera()
  local sx=p.x-cam_x
- local dz_l=40
- local dz_r=128-40-p.w
+ local dz_l,dz_r
+ if p.facing==1 then
+  -- facing right: push player left so more room ahead
+  dz_l=30
+  dz_r=128-55-p.w
+ else
+  -- facing left: push player right
+  dz_l=55
+  dz_r=128-30-p.w
+ end
 
  if sx<dz_l then
   cam_x=p.x-dz_l
